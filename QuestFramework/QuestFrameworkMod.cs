8568f222257cfcd177687ed3e7e850c7f1b9fe4d
Ellen Jana Fawkesová <ellenfawkes@gmail.com>
2023-05-15 10:07:57 +0200

    some multiplayer logging and tweaks

--- QuestFramework/QuestFrameworkMod.cs
+++ QuestFramework/QuestFrameworkMod.cs
@@ -8,6 +8,8 @@ namespace QuestFramework
     public class QuestFrameworkMod : Mod
     {
         private const string MSG_SYNC = "SYNC_QUESTS";
+        private const string SAVE_KEY = "Quests";
+
         private readonly JsonSerializerSettings _jsonSerializerSettings = new();
 
         public override void Entry(IModHelper helper)
@@ -17,6 +19,7 @@ namespace QuestFramework
             helper.Events.GameLoop.UpdateTicked += OnGameUpdated;
             helper.Events.GameLoop.SaveLoaded += OnSaveLoaded;
             helper.Events.GameLoop.Saving += OnSaving;
+            helper.Events.GameLoop.ReturnedToTitle += OnExitToTitle;
         }
 
         private void OnSaving(object? sender, SavingEventArgs e)
@@ -31,7 +34,8 @@ namespace QuestFramework
                 .ToDictionary((m) => m.Key, (m) => m.Value.SaveState(serializer))
             };
 
-            Helper.Data.WriteSaveData("Quests", save);
+            Helper.Data.WriteSaveData(SAVE_KEY, save);
+            Monitor.Log("Quest states for all Quest Managers were successfully saved!", LogLevel.Info);
         }
 
         private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
@@ -39,7 +43,7 @@ namespace QuestFramework
             if (!Context.IsMainPlayer) { return; }
 
             var serializer = JsonSerializer.Create(_jsonSerializerSettings);
-            QuestFrameworkState save = Helper.Data.ReadSaveData<QuestFrameworkState>("Quests") ?? new();
+            QuestFrameworkState save = Helper.Data.ReadSaveData<QuestFrameworkState>(SAVE_KEY) ?? new();
 
             foreach(var farmer in Game1.getAllFarmers())
             {
@@ -51,7 +55,10 @@ namespace QuestFramework
                 }
 
                 QuestManager.Managers.Add(farmer.UniqueMultiplayerID, manager);
+                Monitor.Log($"Loaded Quest Manager state for player '{farmer.Name}' ({farmer.UniqueMultiplayerID})", LogLevel.Debug);
             }
+
+            Monitor.Log($"Successully initialized {QuestManager.Managers.Count} Quest Managers", LogLevel.Info);
         }
 
         private void OnPeerConnected(object? sender, PeerConnectedEventArgs e)
@@ -62,7 +69,12 @@ namespace QuestFramework
 
             if (mod == null || !mod.Version.Equals(ModManifest.Version))
             {
-                Game1.server.kick(e.Peer.PlayerID);
+                Game1.Multiplayer.sendChatMessage(
+                    LocalizedContentManager.CurrentLanguageCode, 
+                    Helper.Translation.Get(
+                        "multiplayer.missMatchVersion", 
+                        new { ClientVersion = mod?.Version, ServerVersion = ModManifest.Version }), 
+                    e.Peer.PlayerID);
                 Monitor.Log($"Mismatch Quest Framework version for peer ${e.Peer.PlayerID}: {mod?.Version} != {ModManifest.Version}", LogLevel.Error);
                 return;
             }
@@ -70,13 +82,22 @@ namespace QuestFramework
             Farmer farmer = Game1.getFarmer(e.Peer.PlayerID); 
             if (farmer == null) return;
 
-            if (!QuestManager.Managers.ContainsKey(e.Peer.PlayerID))
-            {
-                QuestManager.Managers.Add(e.Peer.PlayerID, new QuestManager(farmer));
-            }
-
             var toMods = new string[] { ModManifest.UniqueID };
             var toPlayers = new long[] { e.Peer.PlayerID };
+
+            if (!QuestManager.Managers.ContainsKey(e.Peer.PlayerID))
+            {
+                QuestManager manager = new(farmer);
+                QuestManager.Managers.Add(e.Peer.PlayerID, manager);
+
+                var msg = new QuestSyncMessage(Game1.Multiplayer.writeObjectFullBytes(QuestManager.Managers.Roots[e.Peer.PlayerID], e.Peer.PlayerID), e.Peer.PlayerID)
+                {
+                    Type = QuestSyncMessage.SyncType.FULL
+                };
+                
+                Helper.Multiplayer.SendMessage(msg, MSG_SYNC, toMods);
+                Monitor.Log($"(SYNC) Sent newly added Quest Manager for playerID {msg.FarmerID} to all connected peers");
+            }
 
             foreach (var manager in QuestManager.Managers.Roots)
             {
@@ -86,6 +107,7 @@ namespace QuestFramework
                 };
 
                 Helper.Multiplayer.SendMessage(msg, MSG_SYNC, toMods, toPlayers);
+                Monitor.Log($"(SYNC) Sent Quest Manager full data of playerID {msg.FarmerID} to {string.Join(" ,", toPlayers)}");
             }
         }
 
@@ -99,11 +121,13 @@ namespace QuestFramework
                 {
                     case QuestSyncMessage.SyncType.FULL:
                         QuestManager.Managers.Roots[msg.FarmerID] = Game1.Multiplayer.readObjectFull<QuestManager>(msg.AsReader());
+                        Monitor.Log($"(SYNC) Received Quest Manager full-data for playerID: {msg.FarmerID} Source player: {e.FromPlayerID}");
                         break;
                     case QuestSyncMessage.SyncType.DELTA:
                         if (QuestManager.Managers.Roots.TryGetValue(msg.FarmerID, out var delta))
                         {
                             Game1.Multiplayer.readObjectDelta(msg.AsReader(), delta);
+                            Monitor.Log($"(SYNC) Received Quest Manager delta-data for playerID: {msg.FarmerID} Source player: {e.FromPlayerID}");
                         }
                         break;
                 }
@@ -123,10 +147,25 @@ namespace QuestFramework
                         var msg = new QuestSyncMessage(Game1.Multiplayer.writeObjectDeltaBytes(manager.Value), manager.Key);
                         Helper.Multiplayer.SendMessage(msg, MSG_SYNC, toMods);
                         manager.Value.MarkClean();
+                        Monitor.Log($"(SYNC) Sent delta update for Quest Manager playerID: {msg.FarmerID}");
                     }
                     Game1.Multiplayer.updateRoot(manager.Value);
                 }
             }
+        }
+
+        private void OnExitToTitle(object? sender, ReturnedToTitleEventArgs e)
+        {
+            foreach (var manager in QuestManager.Managers.Values)
+            {
+                if (manager is IDisposable disbosable)
+                {
+                    disbosable.Dispose();
+                }
+            }
+
+            QuestManager.Managers.Clear();
+            Monitor.Log("Quest Managers were uninitialized", LogLevel.Info);
         }
     }
 }
