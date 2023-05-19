917bd4c847afef0f612cf6dc7f844ecc5e8b78c7
Ellen Jana Fawkesová <ellenfawkes@gmail.com>
2023-05-19 16:06:08 +0200

    Prepare patcher system

---
+++ QuestFramework/Framework/Patching/HarmonyPatcher.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using StardewModdingAPI;
+
+namespace QuestFramework.Framework.Patching
+{
+    internal static class HarmonyPatcher
+    {
+        public static Harmony Apply(Mod mod, Patcher[] patchers)
+        {
+            var harmony = new Harmony(mod.ModManifest.UniqueID);
+
+            foreach (var patcher in patchers)
+            {
+                try
+                {
+                    patcher.Apply(harmony, mod.Monitor);
+                }
+                catch (Exception ex)
+                {
+                    mod.Monitor.Log($"Failed to apply '{patcher.GetType().FullName}' patcher; some features may not work correctly. Technical details:\n{ex}", LogLevel.Error);
+                }
+            }
+
+            return harmony;
+        }
+    }
+}
---
+++ QuestFramework/Framework/Patching/Patcher.cs
@@ -0,0 +1,69 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+
+namespace QuestFramework.Framework.Patching
+{
+    internal abstract class Patcher
+    {
+        public abstract void Apply(Harmony harmony, IMonitor monitor);
+
+        protected MethodInfo RequireMethod<TTarget>(string name, Type[]? parameters = null, Type[]? generics = null)
+        {
+            return AccessTools.Method(typeof(TTarget), name, parameters, generics)
+                ?? throw new InvalidOperationException($"Can't find method {GetMethodString(typeof(TTarget), name, parameters, generics)} to patch.");
+        }
+
+        protected ConstructorInfo RequireConstructor<TTarget>(Type[] parameters = null)
+        {
+            return
+                AccessTools.Constructor(typeof(TTarget), parameters)
+                ?? throw new InvalidOperationException($"Can't find constructor {GetMethodString(typeof(TTarget), null, parameters)} to patch.");
+        }
+
+        protected HarmonyMethod GetHarmonyMethod(string name, int priority = -1, string[]? before = null, string[]? after = null, bool? debug = null)
+        {
+            return new HarmonyMethod(
+                AccessTools.Method(GetType(), name)
+                    ?? throw new InvalidOperationException($"Can't find patcher method {GetMethodString(GetType(), name)}."),
+                priority, before, after, debug
+            );
+        }
+
+        private static string GetMethodString(Type type, string? name, Type[]? parameters = null, Type[]? generics = null)
+        {
+            StringBuilder str = new();
+
+            // type
+            str.Append(type.FullName);
+
+            // method name (if not constructor)
+            if (name != null)
+            {
+                str.Append('.');
+                str.Append(name);
+            }
+
+            // generics
+            if (generics?.Any() == true)
+            {
+                str.Append('<');
+                str.Append(string.Join(", ", generics.Select(p => p.FullName)));
+                str.Append('>');
+            }
+
+            // parameters
+            if (parameters?.Any() == true)
+            {
+                str.Append('(');
+                str.Append(string.Join(", ", parameters.Select(p => p.FullName)));
+                str.Append(')');
+            }
+
+            return str.ToString();
+        }
+    }
+}
